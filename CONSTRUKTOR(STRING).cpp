86e0cd4dfe202bc988dd652cf68ff6621541e36a
azissaputr <153833205+azissaputr@users.noreply.github.com>
2024-04-04 05:58:52 +0700

    PRAKTIKUM 3

---
+++ CONSTRUKTOR(STRING).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+ using namespace std;
+ 
+ class pelajaran {		// Class Name
+ 	public :			// Access specifier
+ 		pelajaran (){	// Constructor
+ 			cout << " Ini adalah materi C++ tentang Constructors! ";
+		 }
+ };
+ 
+ 
+ int main (){
+ 	pelajaran obj;		// membuat object dari sebuah
+
+ 	return 0;
+ }
